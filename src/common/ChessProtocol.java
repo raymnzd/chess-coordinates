b022601b75611f9c52e8a7d0f07a22ded6c398d2
raymond zhao <raymnzd@gmail.com>
2019-08-14 17:07:37 -0400

    added protocol

---
+++ src/common/ChessProtocol.java
@@ -0,0 +1,16 @@
+package common;
+
+public interface ChessProtocol {
+
+
+    public static final String WELCOME =  "WELCOME";
+
+    public static final String CLICK = "CLICK";
+
+    public static final String NEW_COORD = "NEW";
+
+
+
+
+}
+
