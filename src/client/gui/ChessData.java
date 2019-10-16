74f7f508a58a64bd56eeab44bd2ee3e34982e8c8
raymond zhao <raymnzd@gmail.com>
2019-10-15 23:23:23 -0400

    added documentation to ChessData

--- src/client/gui/ChessData.java
+++ src/client/gui/ChessData.java
@@ -10,26 +10,28 @@ package client.gui;
 
 public class ChessData {
 
+    //holds current coordinate
     private String coord;
 
+    //holds player's score
     private int score;
 
-
-
+    //sets new coordinate
     public void setCoord(String coord){
         this.coord = coord;
     }
 
-
+    //gets current coordinate
     public String getCoord(){
         return coord;
     }
 
-
+    //sets player's score
     public void setScore(int score){
         this.score = score;
     }
 
+    //get player's score
     public int getScore(){
         return score;
     }
